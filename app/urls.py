ecb2ad31ed65cd1bbd9c75c68f0c3900d91205c4
Holger Dobosz <hdobosz@freenet.de>
2023-10-20 20:35:14 +0200

    changes

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, user_login, user_logout
+from .views import register, user_login, user_logout, HomePageView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
