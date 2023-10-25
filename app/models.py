1718572432227e0cd006d9a1b5d9454f675fd882
Holger Dobosz <hdobosz@freenet.de>
2023-10-25 19:29:30 +0200

    new model

--- app/models.py
+++ app/models.py
@@ -15,3 +15,15 @@ class EnergyConsumption(models.Model):
 
     def __str__(self):
         return f"{self.year} - {self.month}"
+
+class BuildingConsumption(models.Model):
+    total_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+    water_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+    house_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+    ground_floor_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+    attic_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+    basement_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+    heating_consumption = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Total Consumption: {self.total_consumption}\nWater Consumption: {self.water_consumption}\nHouse Consumption: {self.house_consumption}\nGround Floor Consumption: {self.ground_floor_consumption}\nAttic Consumption: {self.attic_consumption}\nBasement Consumption: {self.basement_consumption}\nHeating Consumption: {self.heating_consumption}"
